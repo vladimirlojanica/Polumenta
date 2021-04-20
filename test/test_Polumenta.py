228fe1d6e00c78cdfb440de06d11f012a0411784
Vladimir <vladimirlojanica@gmail.com>
2021-04-20 14:52:08 +0200

    grouped asserts in test

--- test/test_Polumenta.py
+++ test/test_Polumenta.py
@@ -1,17 +1,12 @@
 import pytest
 from polumenta.generator import jedanPolumenta, Polumente, rodiPolumentu
 
-def test_jedanPolumenta_works():	
+def test_jedanPolumenta():	
 	pol = jedanPolumenta()
 	assert(pol != '')
-
-def test_jedanPolumenta_returnsAtLeast4Chars():	
-	pol = jedanPolumenta()
 	assert(len(pol) > 3)
-
-def test_jedanPolumenta_returnsOneName():	
-	pol = jedanPolumenta()
 	assert(len(pol.split()) == 1)
+
 
 def test_Polumente_low():
 	poli = Polumente(-20)
