3290eca60e643e54a01c92fa0222cb12b9bb8b95
Vladimir <vladimirlojanica@gmail.com>
2021-04-21 09:59:18 +0200

    added setup.py

---
+++ setyp.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='pylumenta',
+	version='0.1.2',
+	description='Polumenta generator',
+	author='Vladimir Lojanica',
+	author_email='vladimirlojanica@gmail.com',
+	url='https://github.com/vladimirlojanica/pylumenta',
+	packages=find_packages(),
+	install_requires=[],
+	extras_require={},
+	setup_requires=[],
+	tests_require=['pytest'],
+	entry_points={},
+	package_data={}
+)
